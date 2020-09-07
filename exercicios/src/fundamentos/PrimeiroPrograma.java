3bc2bcae8c3904a4c691576f88c08837cc130de4
Muller Marciel <mullermbm@gmail.com>
2020-09-06 23:48:59 -0300

    teste git

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -5,5 +5,6 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Primeiro programa parte 1");
 		System.out.println("Primeiro programa parte 2");
+		System.out.println("Primeiro programa parte 3");
 	}
 }
