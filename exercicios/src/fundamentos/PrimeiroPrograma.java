be79621dcbbc8ce51a22e58db20036a470ba2641
Muller Marciel <mullermbm@gmail.com>
2020-09-13 19:34:46 -0300

    novo teste

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,30 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Muller
+ * @since JDK2.0
+ */
+
 public class PrimeiroPrograma {
+	
+	/*
+	 * @param args
+	 */
+	
 
 	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
+		// Mais um comentário...
+		// fim
 		System.out.println("Primeiro programa parte 1");
+		
+		/*
+		  Linha 1
+		  Linha 2
+		  Linha 3...
+		 */
 		System.out.println("Primeiro programa parte 2");
 		System.out.println("Primeiro programa parte 3");
 	}
